a3ae088175ed67762aba2ebc9da260aae8d36f62
Markus Hjort <markus.hjort@gmail.com>
2018-05-12 17:17:23 +0300

    Bump highcharts-reporter

--- project.clj
+++ project.clj
@@ -5,7 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.amazonaws/aws-java-sdk-s3 "1.10.50"]
-                 [clojider-gatling-highcharts-reporter "0.2.1"]]
-  :profiles {:dev {:dependencies [[clj-containment-matchers "1.0.1"]] }})
-
+                 [clojider-gatling-highcharts-reporter "0.2.2"]])
 
